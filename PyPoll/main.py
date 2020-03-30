e3e68a84416b9021bc76da7aaaacda1dd86f2bec
cdrake11 <chantay.drake@gmail.com>
2020-03-29 23:46:10 -0400

    Update main.py
    I have been battling a cold all week so I have been under the weather. So I will turn in Pypoll Tomorrow is it goes throguh the deadline

---
+++ PyPoll/main.py
@@ -0,0 +1,29 @@
+import os
+Import csv
+#Directions..
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+#Election Results
+#-------------------------
+#Total Votes: 3521001
+#-------------------------
+#Khan: 63.000% (2218231)
+#Correy: 20.000% (704200)
+#Li: 14.000% (492940)
+#O'Tooley: 3.000% (105630)
+#-------------------------
+#Winner: Khan
+election_data = os.path.join('.','election_data')
+
+
+
+
+# Declare my variables
+total_votes=[]
+
+
+print
+
